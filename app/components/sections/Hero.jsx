5721a471c07b142cc9114bee7d07d019ac5eab24
Abhee <abhaynimbalkar03@gmail.com>
2026-01-11 14:56:46 +0530

    created a skeleton

---
+++ app/components/sections/Hero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HeroSection() {
+    return (
+        <div className='h-screen w-full flex items-center justify-center'>HeroSection</div>
+    )
+}
+
+export default HeroSection
---
+++ app/components/sections/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Nav() {
+    return (
+        <div className='w-full fixed py-10 flex items-center justify-center text-xl'>Nav</div>
+    )
+}
+
+export default Nav
